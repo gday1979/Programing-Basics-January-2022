9763778366e22307a67d1a96ccedb87a13464d0a
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-08-19 19:01:21 +0300

    commit

--- 07. Trekking Mania/Program.cs
+++ 07. Trekking Mania/Program.cs
@@ -6,7 +6,48 @@ namespace _07._Trekking_Mania
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int numberOfGroups=int.Parse(Console.ReadLine());
+            int g1 = 0;
+            int g2 = 0;
+            int g3 = 0;
+            int g4 = 0;
+            int g5 = 0;
+          
+            for (int i = 1; i <=numberOfGroups; i++)
+            {
+                int cliffhanger = int.Parse(Console.ReadLine());
+                if (cliffhanger<6)
+                {
+                    g1 += cliffhanger;
+                }
+                else if (cliffhanger<13)
+                {
+                    g2 +=cliffhanger;
+                }
+                else if (cliffhanger<26)
+                {
+                    g3 += cliffhanger;
+
+                }
+                else if ( cliffhanger<41)
+                {
+                    g4 += cliffhanger;
+                }
+                else 
+                {
+                    g5 += cliffhanger;
+              
+                }
+                
+            }
+            int totalGroups = g1 + g2 + g3 + g4 + g5;
+            Console.WriteLine($"{1.0*g1/totalGroups*100:f2}%");
+            Console.WriteLine($"{1.0*g2/totalGroups*100:f2}%");
+            Console.WriteLine($"{1.0*g3/totalGroups*100:f2}%");
+            Console.WriteLine($"{1.0*g4/totalGroups*100:f2}%");
+            Console.WriteLine($"{1.0*g5/totalGroups*100:f2}%");
+
+
         }
     }
 }
--- 08. Tennis Ranklist/Program.cs
+++ 08. Tennis Ranklist/Program.cs
@@ -6,10 +6,44 @@ namespace _08._Tennis_Ranklist
     {
         static void Main(string[] args)
         {
-            for (int i = 1; i <=100; i++)
+            int numTour = int.Parse(Console.ReadLine());
+            int startPoints = int.Parse(Console.ReadLine());
+
+            double sumW = 0;
+            double sumF = 0;
+            double sumSf = 0;
+            double numW = 0;
+
+            for (int i = 1; i <= numTour; i++)
             {
-                Console.WriteLine(i);
+                string lvl = Console.ReadLine();
+
+                if (lvl == "W")
+                {
+                    sumW = sumW + 2000;
+                    numW++;
+                }
+                else if (lvl == "F")
+                {
+                    sumF += 1200;
+                }
+                else if (lvl == "SF")
+                {
+                    sumSf += 720;
+                }
+
             }
+            double prWins = numW / numTour * 100;
+            double sumFromTour = sumSf + sumW + sumF;
+            double allPoints = sumSf + sumW + sumF + startPoints;
+            double averagePoints = Math.Floor(sumFromTour / numTour);
+
+
+            Console.WriteLine($"Final points: {allPoints}");
+            Console.WriteLine($"Average points: {averagePoints}");
+            Console.WriteLine($"{prWins:f2}%");
+
+
         }
     }
 }
