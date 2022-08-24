1d42557d3d1dfc5fca868a55a7cd5bb264a83f1d
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-08-24 20:31:35 +0300

    commit

--- 05. Account Balance/Program.cs
+++ 05. Account Balance/Program.cs
@@ -6,7 +6,21 @@ namespace _05._Account_Balance
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input=Console.ReadLine();
+            double sum = 0;
+            while (input !="NoMoreMoney")
+            {
+                double amount=double.Parse(input);
+                if (amount<0)
+                {
+                    Console.WriteLine("Invalid operation!");
+                    break;
+                }
+                sum+=amount;
+                Console.WriteLine($"Increase: {amount:f2}");
+                input= Console.ReadLine();
+            }
+            Console.WriteLine($"Total: {sum:f2}");
         }
     }
 }
