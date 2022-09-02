834a84dff666ae92f8f05500450cfb62572f1da1
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-02 19:01:54 +0300

    commit

---
+++ 03. Combinations/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _03._Combinations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n=int.Parse(Console.ReadLine());
+            int counter = 0;
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    for (int y = 0; y <= n; y++)
+                    {
+                        if (i+j+y==n)
+                        {
+                            counter++;
+                        }
+                    }
+                }
+            }
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ 04. Sum of Two Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _04._Sum_of_Two_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOne = int.Parse(Console.ReadLine());
+            int numberTwo = int.Parse(Console.ReadLine());
+            int magicNumber = int.Parse(Console.ReadLine());
+            int counter = 0;
+            bool isMagicNumberFound = false;
+            for (int i = numberOne; i <= numberTwo; i++)
+            {
+                for (int j = numberOne; j <= numberOne; j++)
+                {
+                    counter++;
+
+
+                    if (numberOne + numberOne == magicNumber)
+                    {
+                        isMagicNumberFound = true;
+                        Console.WriteLine($"Combination N:{counter} ({numberOne} + {numberTwo} = {magicNumber}");
+                        return;
+                    }
+                }
+            }
+            if (!isMagicNumberFound)
+            {
+                Console.WriteLine($"{counter} combinations - neither equals {magicNumber}");
+            }
+        }
+    }
+}
