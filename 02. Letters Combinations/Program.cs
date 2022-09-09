0072ece539a22601d9d7b5aae640f05f7918e26e
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-09 19:26:13 +0300

    commit

--- 02. Letters Combinations/Program.cs
+++ 02. Letters Combinations/Program.cs
@@ -6,7 +6,25 @@ namespace _02._Letters_Combinations
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+            char c = char.Parse(Console.ReadLine());
+            int combination = 0;
+            for (char i = a; i <= b; i++)
+            {
+                for (char j = a; j <= b; j++)
+                {
+                    for (char k = a; k <= b; k++)
+                    {
+                        if (i != c && j != c && k != c)
+                        {
+                            Console.Write("{0}{1}{2} ", i, j, k);
+                            combination++;
+                        }
+                    }
+                }
+            }
+            Console.Write(combination + " ");
         }
     }
 }
--- 03. Lucky Numbers/Program.cs
+++ 03. Lucky Numbers/Program.cs
@@ -6,7 +6,26 @@ namespace _03._Lucky_Numbers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n=int.Parse(Console.ReadLine());
+            for (int i = 1; i <=9; i++)
+            {
+                for (int j = 1; j <=9; j++)
+                {
+                    for (int k = 1; k <=9; k++)
+                    {
+                        for (int m = 1; m <=9; m++)
+                        {
+                            if ((i+j)==(k+m))
+                            {
+                                if (n%(i+j)==0)
+                                {
+                                    Console.Write(""+i + j + k + m + " ");
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 }
