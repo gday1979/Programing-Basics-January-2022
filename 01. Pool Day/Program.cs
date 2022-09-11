a22056c34f82fbd071757c3e064781bdf609515a
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-11 13:17:22 +0300

    commit

--- 01. Pool Day/Program.cs
+++ 01. Pool Day/Program.cs
@@ -6,7 +6,14 @@ namespace _01._Pool_Day
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double people=double.Parse(Console.ReadLine());
+            double priceEnter=double.Parse(Console.ReadLine());
+            double priceSunLoger=double.Parse(Console.ReadLine());  
+            double priceUmbrela=double.Parse(Console.ReadLine());
+            double sunLoger = people * 0.75;
+            double umbrella = people * 0.50;
+            double sum=(people*priceEnter)+(priceSunLoger*sunLoger)+(priceUmbrela*umbrella);
+            Console.WriteLine($"{sum:f2} lv.");
         }
     }
 }
