8ba6820355a40fff9ce0888de90a8af296db879b
gday1979 <97783032+gday1979@users.noreply.github.com>
2022-09-26 18:02:51 +0300

    commit

--- 04. Tourist Shop/Program.cs
+++ 04. Tourist Shop/Program.cs
@@ -6,7 +6,27 @@ namespace _04._Tourist_Shop
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double budget = double.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            int products = 0;
+            double total = 0;
+
+            while (input != "Stop")
+            {
+                double price = double.Parse(Console.ReadLine());
+                if ((products + 1) % 3 == 0) price /= 2;
+                total += price;
+                if (budget < total) break;
+                products++;
+                input = Console.ReadLine();
+            }
+
+            if (budget < total)
+            {
+                Console.WriteLine($"You don't have enough money!");
+                Console.WriteLine($"You need {(total - budget):f2} leva!");
+            }
+            else Console.WriteLine($"You bought {products} products for {total:f2} leva.");
         }
     }
 }
--- 05. Division Without Remainder/Program.cs
+++ 05. Division Without Remainder/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _05._Division_Without_Remainder
 {
@@ -6,7 +7,29 @@ namespace _05._Division_Without_Remainder
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int n = int.Parse(Console.ReadLine());
+            double p1 = 0;
+            double p2 = 0;
+            double p3 = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double numbers = double.Parse(Console.ReadLine());
+                if (numbers % 2 == 0)
+                {
+                    p1++;
+                }
+                if (numbers % 3 == 0)
+                {
+                    p2++;
+                }
+                if (numbers % 4 == 0)
+                {
+                    p3++;
+                }
+            }
+            Console.WriteLine($"{p1/n*100:f2}%");
+            Console.WriteLine($"{p2/n*100:f2}%");
+            Console.WriteLine($"{p3/n*100:f2}%");
         }
     }
 }
